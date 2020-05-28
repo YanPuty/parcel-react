fc0f687bca6e474d224bc3dac47a287d791b30aa
yanputy <puty.yan@sparkdigital.asia>
2020-05-28 22:02:40 +0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "React";
+import ReactDOM from "react-dom";
+
+import { App } from "./view";
+
+// style
+import "./styles/index.scss";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
---
+++ src/view/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "antd";
+
+export class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="container">
+               <div>Hello World</div> 
+            </div>
+        );
+    };
+}
